77b016d1d380802bcc3cc4b8c2aaf35074395999
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2025-02-10 22:14:48 -0300

    add full data

--- run_cramming_pt.sh
+++ run_cramming_pt.sh
@@ -1,6 +1,13 @@
-python pretrain.py\
+!python cramming/pretrain.py\
    name=cramming-bert-pt\
    arch=crammed-bert\
    train=bert-o4\
-   data=giga-verbo\
-   dryrun=True\
+   data=giga-verbo-full\
+   wandb.enabled=True\
+   wandb.entity=...\
+   wandb.project=...\
+   wandb.tags=[full,crammed,pt]\
+   budget=24\
+   impl.microbatch_size=64\
+   impl.batch_size=4096\
+   impl.gradient_accumulation_steps=16
